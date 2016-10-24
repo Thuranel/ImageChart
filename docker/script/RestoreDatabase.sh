c3e910ec92c3e72bd6a06f37ba0b627b8801adb0
Étienne Gélinas-Gagnon <etiennegg7@hotmail.fr>
2016-10-24 16:13:44 -0400

    ajout du Docker pour obtenir les données

---
+++ docker/script/RestoreDatabase.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -m
+apt-get update
+cd championweb
+git pull origin master
+
+mongodb_cmd="mongod --storageEngine $STORAGE_ENGINE"
+cmd="$mongodb_cmd --httpinterface --rest --master"
+
+if [ "$OPLOG_SIZE" != "" ]; then
+    cmd="$cmd --oplogSize $OPLOG_SIZE"
+fi
+
+$cmd &
+
+if [ ! -f /data/db/.mongodb_password_set ]; then
+    /set_mongodb_password.sh
+fi
+
+
+echo "Actualisation des donnees"
+mongorestore --db championgg --collection webchampionpages --drop db/championgg/webchampionpages.bson
+mongorestore --db championgg --collection webchampionroles --drop db/championgg/webchampionroles.bson
+mongorestore --db championgg --collection webmatchuppages --drop db/championgg/webmatchuppages.bson
+mongorestore --db championgg --collection weboverallroledatas --drop db/championgg/weboverallroledatas.bson
+mongorestore --db championgg --collection weboverallstats --drop db/championgg/weboverallstats.bson
+mongorestore --db championgg --collection webhomepagesummaries --drop db/championgg/webhomepagesummaries.bson
+mongorestore --db championgg --collection webstatisticspages --drop db/championgg/webstatisticspages.bson
+
+PORT=8888 npm start
+
+fg
